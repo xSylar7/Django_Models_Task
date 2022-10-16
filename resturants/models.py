6a4f11a57063c75274d9a6c41473996aedee782d
Qassem Alsaffar <qas.alsaffar@gmail.com>
2022-10-16 10:29:44 +0300

    fixed description defult value

--- resturants/models.py
+++ resturants/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+from email.policy import default
 from django.db import models
 
 # Create your models here.
 class Resturant (models.Model):
     name = models.CharField(max_length = 30)
-    description = models.TextField()
+    description = models.TextField(default="", blank=True)
     opening_time = models.TimeField()
     closing_time = models.TimeField()
     created_at = models.DateTimeField(auto_now_add = True)
